cf5e0edf1baeb66d0aa4c06a99db11fbf7a01d9a
Youssef El Shabrawii <youssef.elshabrawii@gmail.com>
2024-01-23 13:45:47 +0200

    Edit Configure WiFi

--- src/PI/Wifi_Conf.sh
+++ src/PI/Wifi_Conf.sh
@@ -3,6 +3,7 @@
 if [ "$1" == "hotspot" ]; then
     # Hotspot Configuration
     sudo service NetworkManager start
+    sleep 2
     sudo nmcli device wifi hotspot ssid Momentum-wcr password momentum_2024 ifname wlan0
 
 elif [ "$1" == "normalwifi" ]; then
