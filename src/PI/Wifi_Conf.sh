1c5f516a7f0f556defa342f82e7f96134f5fa65e
Youssef El Shabrawii <youssef.elshabrawii@gmail.com>
2024-01-23 13:39:24 +0200

    Edit Configure WiFi

--- src/PI/Wifi_Conf.sh
+++ src/PI/Wifi_Conf.sh
@@ -2,41 +2,14 @@
 
 if [ "$1" == "hotspot" ]; then
     # Hotspot Configuration
-    echo "Configuring Raspberry Pi as Hotspot..."
-
-    # Stop services
-    sudo systemctl stop hostapd
-    sudo systemctl stop dnsmasq
-
-    # Edit network interfaces file
-    sudo cp /etc/network/interfaces /etc/network/interfaces.backup
-    sudo echo -e "auto lo\niface lo inet loopback\niface eth0 inet dhcp" | sudo tee /etc/network/interfaces
-
-    # Start services
-    sudo systemctl start hostapd
-    sudo systemctl start dnsmasq
-
-    echo "Hotspot configuration complete. Rebooting..."
-    sudo reboot
+    sudo service NetworkManager start
+    sudo nmcli device wifi hotspot ssid Momentum-wcr password momentum_2024
 
 elif [ "$1" == "normalwifi" ]; then
     # Normal Wi-Fi Configuration
-    echo "Configuring Raspberry Pi as Normal Wi-Fi..."
-
-    # Stop services
-    sudo systemctl stop hostapd
-    sudo systemctl stop dnsmasq
-
-    # Restore original network interfaces file
-    sudo cp /etc/network/interfaces.backup /etc/network/interfaces
-
-    # Start services
-    sudo systemctl start hostapd
-    sudo systemctl start dnsmasq
-
-    echo "Normal Wi-Fi configuration complete. Rebooting..."
-    sudo reboot
-
+    sudo service NetworkManager stop
+    sudo service dhcpcd restart
+    
 else
     echo "Invalid parameter. Use 'hotspot' or 'normalwifi'."
 fi
